71a5a39b9836c21908b57b81e8532dc4d254578a
ramuked <sushantkumar2025@gmail.com>
2023-01-25 19:47:04 +0530

    Added food item update forms

--- food/urls.py
+++ food/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     # /food/1
     path("<int:item_id>", views.detail, name="detail"),
     path("item/", views.item, name="item"),
+    path("add", views.create_item, name="create_item"),
+    path("update/<int:id>", views.update_item, name="update_item"),
+    path("delete/<int:id>", views.delete_item, name="delete_item"),
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from food.forms import ItemForm
 from .models import Item
 from django.template import loader
 
@@ -23,3 +25,31 @@ def detail(request, item_id):
         "item": item,
     }
     return render(request, "food/detail.html", context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+    return render(request, "food/item-form.html", {"form": form})
+
+
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+    return render(request, "food/item-form.html", {"form": form, "item": item})
+
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("food:index")
+
+    return render(request, "food/item-delete.html", {"item": item})
